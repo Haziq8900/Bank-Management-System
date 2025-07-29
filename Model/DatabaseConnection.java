4f5858e411a5fe2693761de7b653272aa39f21b1
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-07-29 22:24:50 +0500

    Database file and project structure updated

---
+++ Model/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String databaseName = "ATM_DB"; // change it accordingly
+    private static final String URL = "jdbc:mysql://localhost:3306/" + databaseName;
+    private static final String USER = "root";  // change it accordingly
+    private static final String PASSWORD = "ahtisham123";  // change it accordingly
+
+    static {
+        try {
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load MySQL JDBC driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        // Return a new database connection
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            if (connection != null) {
+                System.out.println("Database connection successful!");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());;
+        }
+    }
+}

31ad86ac6954d4668770238759f79c853367ebdc
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-07-30 15:50:56 +0500

    Updated Main file

--- Main.java
+++ Main.java
@@ -1,15 +1,10 @@
-import Backend.ATMSystem;
-import Backend.User;
 import View.MainFrame;
 
 import javax.swing.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        SwingUtilities.invokeLater(() -> {
-            new MainFrame();
-        });
+        SwingUtilities.invokeLater(MainFrame::new);
     }
 }

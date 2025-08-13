7bea1b54c6abaff0090c0a723f18a1697b104dd4
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-08-13 18:47:35 +0500

    Acheivement commit
    Co-authored-by: Haziq8900 <haziqkhan8900@gmail.com>

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        // The software runs from this class
         SwingUtilities.invokeLater(MainFrame::new);
     }
 }

491632eb55901a789c75d2e82443d6e1309c164b
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-08-21 11:21:09 +0500

    Updated Account class

--- Backend/Account.java
+++ Backend/Account.java
@@ -1,18 +1,31 @@
 package Backend;
 
+import java.util.Random;
+
 public class Account {
-
-
     private String account_number;
     private String name;
     private String cnic;
     private String phone;
-    private String account_type;
+    private byte account_type;
     private double balance;
+    private String address;
 
-    
+    public Account(String name, String cnic, String phone, byte account_type, String address) {
+        this.address = address;
+        this.name = name;
+        this.cnic = cnic;
+        this.phone = phone;
+        this.account_type = account_type;
+        this.setAccount_number();
+    }
+
     // Getter Methods
 
+
+    public String getAddress() {
+        return address;
+    }
 
     public String getName() {
         return name;
@@ -30,7 +43,7 @@ public class Account {
         return account_number;
     }
 
-    public String getAccount_type() {
+    public byte getAccount_type() {
         return account_type;
     }
 
@@ -41,6 +54,10 @@ public class Account {
 
     // Setter Methods
 
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -54,11 +71,12 @@ public class Account {
         this.phone = phone;
     }
 
-    public void setAccount_number(String account_number) {
-        this.account_number = account_number;
+    private void setAccount_number() {
+        int random = new Random().nextInt(1000);
+        this.account_number = this.getCnic() + random;
     }
 
-    public void setAccount_type(String account_type) {
+    public void setAccount_type(byte account_type) {
         this.account_type = account_type;
     }
 
